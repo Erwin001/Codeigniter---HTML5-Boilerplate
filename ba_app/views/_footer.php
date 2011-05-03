9f3aeeb2a3305b51b259dbab3b6d0f0deb8b8299
Serkan Sökmen <e.serkan.sokmen@gmail.com>
2011-05-03 17:50:18 +0300

    jQuery 1.6 update

--- ba_app/views/_footer.php
+++ ba_app/views/_footer.php
@@ -6,7 +6,7 @@
 	
 	<!-- JavaScript at the bottom for fast page loading -->
   <!-- jQuery -->
-	<script src="<?php echo base_url().JSLIB_FOLDER; ?>jquery-1.5.2.min.js"></script>
+	<script src="<?php echo base_url().JSLIB_FOLDER; ?>jquery-1.6.min.js"></script>
 	
 	<!-- scripts concatenated and minified via ant build script-->
 	<script src="<?php echo base_url().JS_FOLDER; ?>plugins.js"></script>
